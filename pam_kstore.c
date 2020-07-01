c73f52f0a90cff90909c929431fef42923571cf5
Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>
2020-07-01 09:45:18 +0200

    Updated copyright

--- pam_kstore.c
+++ pam_kstore.c
@@ -1,10 +1,22 @@
-/* pam_keystore module, based on pam_tmpdir module which in turn was based on
-   pam_env which in turn was based on pam_mail */
-
 /*
+ * Copyright (C) 2020 Canonical
+ *
  * Authors:
- *  Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>   2020-06-18
- *  Didier Roche <didrocks@ubuntu.com>  2020-06-18
+ *  Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>
+ *  Didier Roche <didrocks@ubuntu.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 #define _GNU_SOURCE
